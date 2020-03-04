63e6e0bcd413ee192c6afb3db5346b4df90d19d1
thomas <thomasmckanna@gmail.com>
2020-03-04 17:58:10 -0600

    Updated DB user to 'postgres'.

--- rolla_mission/rolla_mission/settings.py
+++ rolla_mission/rolla_mission/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mission',
-        'USER': 'admin',
+        'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
