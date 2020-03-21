9ce2009377952eb9dcd5d4bdd23b49dbadb86b66
daniel <rozhanskiyd@hotmail.com>
2020-03-20 22:09:57 -0500

    updated admin and model files to include a calculated time since homeless field in the admin screen

--- code/patron_checkin/admin.py
+++ code/patron_checkin/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
+from django.utils.timesince import timesince
 
 from .models import Patron, CheckIn
 
@@ -14,11 +15,11 @@ class PatronAdmin(admin.ModelAdmin):
             'fields': ('veteran', 'violence', 'offender')
         }),
         ('History', {
-            'fields': ('time_homeless', 'city', 'reason', 'other')
+            'fields': ('duration_homeless', 'date_homeless', 'city', 'reason', 'other')
         })
     )
 
-    readonly_fields = ["headshot_img", "signature_img"]
+    readonly_fields = ["headshot_img", "signature_img", 'duration_homeless']
 
     def headshot_img(self, obj):
         return mark_safe('<img src="{url}" width="150" height="240" />'.format(
@@ -33,3 +34,7 @@ class PatronAdmin(admin.ModelAdmin):
         )
     )
     signature_img.short_description = 'Signature'
+
+    def duration_homeless(self, obj):
+        return timesince(obj.date_homeless)
+    duration_homeless.short_description = 'Duration Homeless'
