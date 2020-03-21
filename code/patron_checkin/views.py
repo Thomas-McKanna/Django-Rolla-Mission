9bf602dc4cbfb19202f006e8bade0ae527c42cfd
thomas <thomasmckanna@gmail.com>
2020-03-21 15:14:50 -0500

    Added authentication to API endpoints.

--- code/patron_checkin/views.py
+++ code/patron_checkin/views.py
@@ -5,21 +5,26 @@ from patron_checkin.serializers import SignatureSerializer
 from patron_checkin.serializers import CheckInSerializer
 from rest_framework import generics
 from rest_framework.parsers import JSONParser, MultiPartParser
-
+from rest_framework import permissions
 
 class PatronList(generics.ListCreateAPIView):
     queryset = Patron.objects.all()
     serializer_class = ExtendedPatronSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class UpdateHeadshot(generics.RetrieveUpdateAPIView):
     queryset = Patron.objects.all()
     serializer_class = HeadshotSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class UpdateSignature(generics.RetrieveUpdateAPIView):
     queryset = Patron.objects.all()
     serializer_class = SignatureSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
 
 class CheckInPatron(generics.CreateAPIView):
     serializer_class = CheckInSerializer
+    permission_classes = [permissions.IsAuthenticated]
