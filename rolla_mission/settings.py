3eeebfe56334692a36a9e85c00069f7ed354bfdb
Thomas McKanna <tjmzq8@mst.edu>
2020-04-11 22:27:36 +0000

    Removed token authentication and simply enabled authentication header forwarding in apache conf file.

--- rolla_mission/settings.py
+++ rolla_mission/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'storages',
     'rest_framework',
-    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -109,12 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
