b984cc3f36c9fc1ac424f00f610164bfe810ce19
thomas <thomasmckanna@gmail.com>
2020-04-18 19:59:25 -0500

    Example report for all patrons.

--- patron_checkin/urls.py
+++ patron_checkin/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('pdf', views.html_to_pdf_view, name='report')
 ]
--- patron_checkin/views.py
+++ patron_checkin/views.py
@@ -3,6 +3,10 @@ from django.views import generic
 from datetime import datetime, timedelta
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from weasyprint import HTML
+
 
 class IndexView(LoginRequiredMixin, generic.ListView):
     template_name = 'patron_checkin/index.html'
@@ -11,3 +15,15 @@ class IndexView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         time_threshold = datetime.now() - timedelta(hours=12)
         return Patron.objects.filter(checkin__date__gt=time_threshold).order_by('-name')
+
+
+def html_to_pdf_view(request):
+    patrons = Patron.objects.all()
+    html_string = render_to_string(
+        'patron_checkin/report.html', {'patrons': patrons})
+
+    html = HTML(string=html_string)
+
+    response = HttpResponse(html.write_pdf(), content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="patron_report.pdf"'
+    return response
